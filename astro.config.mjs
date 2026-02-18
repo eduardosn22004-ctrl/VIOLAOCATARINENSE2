46b43b9ed4d10d9d0bb2d63cead60047d9efc376
eduardosn22004-ctrl <eduardosn22004@gmail.com>
2026-02-18 15:01:04 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,14 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+import vercel from '@astrojs/vercel'; // Trocamos netlify por vercel
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'server', // Adicionado para suportar funções serverless na Vercel
     vite: {
         plugins: [tailwindcss()]
     },
     integrations: [react()],
-    adapter: netlify({
-        devFeatures: {
-            environmentVariables: true
-        }
-    })
+    adapter: vercel() // Configurado para a Vercel
 });
