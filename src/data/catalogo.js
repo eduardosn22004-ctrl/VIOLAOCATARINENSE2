36eeb563b10aecb8a639d98ebb6340a1e64727d5
eduardosn22004-ctrl <eduardosn22004@gmail.com>
2026-03-09 14:13:53 -0300

    Update catalogo.js

--- src/data/catalogo.js
+++ src/data/catalogo.js
@@ -1,1 +1,18 @@
-export const musicos =[
+export const catalogo = [
+  {
+    id: "edino-krieger", // LIGAÇÃO COM A BIOGRAFIA DO EDINO
+    compositor: "Edino Krieger",
+    obras: [
+      { titulo: "Ritmata", ano: "1974", formacao: "Violão Solo", linkYoutube: "https://youtube.com/watch?v=exemplo" },
+      { titulo: "Romance", ano: "1953", formacao: "Violão Solo", linkYoutube: "" }
+    ]
+  },
+  {
+    id: "villallobos", // LIGAÇÃO COM A BIOGRAFIA DO MÁRIO
+    compositor: "Heitor Villa-Lobos",
+    obras: [
+      { titulo: "Estudo Noº 3", ano: "1998", formacao: "Violão Solo", linkYoutube: "" }
+    ]
+  }
+];
+
