3238acb4043265e92b6145f449363bbc20ed73e3
eduardosn22004-ctrl <eduardosn22004@gmail.com>
2026-03-13 17:38:21 -0300

    Update arranjo.js

--- src/data/arranjo.js
+++ src/data/arranjo.js
@@ -1,1 +1,31 @@
-
+export const catalogo = [
+{
+    id: "darcy-villa-verde",
+    compositor: "Darcy Villa Verde",
+    obras: [
+      { titulo: "Foi ela - Ary Barroso", ano: "", formacao: "todas peças são arranjos", linkYoutube: "" },
+      { titulo: "Eu Sonhei Que Tu Estavas Tão Linda", ano: "", formacao: "f barroso,(faleceu)", linkYoutube: "" },
+      { titulo: "A Felicidade - Tom Jobim", ano: "", formacao: "Darcy Villa Verde / Foi Ela / Eu Sonhei Que Tu Estavas Tão / Felicidade / Se Tu Soubesses", linkYoutube: "" },
+      { titulo: "Se Tu Soubesse - cristóvão de alencar", ano: "", formacao: "VCFZ 89 - Dagoberto Linhares, Darcy Villa Verde (2007)", linkYoutube: "" }
+    ]
+  },
+{
+    id: "ricardob",
+    compositor: "Ricardo Boppre",
+    obras: [
+      { titulo: "O Velinho", ano: "", formacao: "(arranjo)", linkYoutube: "https://youtu.be/c4C3aFmF27M?si=3KJQ0b0cG2YUWzct" },
+      { titulo: "O Holy Night", ano: "", formacao: "(arranjo)", linkYoutube: "https://youtu.be/7faW2fTG9c0?si=hdl72HT8kmLV53GC" },
+      { titulo: "Noite Feliz", ano: "", formacao: "(arranjo)", linkYoutube: "https://youtu.be/JffDw32h7Mo?si=em5Cu6hK2c6jK4UF" }
+      { titulo: "Natal das Crianças", ano: "", formacao: "(arranjo)", linkYoutube: "https://youtu.be/7RkakszxvB8?si=qr42HlJdnlAdRFAA" },
+      { titulo: "Jingle Bells", ano: "", formacao: "(arranjo)", linkYoutube: "https://youtu.be/HmsZTyzvWHY?si=JbYApNQEv_y4rkgE" },
+      { titulo: "Hark the Herald Angels Sing", ano: "", formacao: "(arranjo)", linkYoutube: "https://youtu.be/AfR3JZFI00o?si=QxOIb2tY8oa7E1N-" },
+      { titulo: "Como é Belo o Natal", ano: "", formacao: "(arranjo)", linkYoutube: "https://youtu.be/v--9FExRTF8?si=fASE6NKtfikh8qLW" },
+  ]
+  },
+{
+    id: "vitorgar",
+    compositor: "Vitor Garbelotto",
+    obras: [
+      { titulo: "Radamés Gnattalli", ano: "2010", formacao: "todas peças sao arranjos", linkYoutube: "" },
+    ]
+  }
