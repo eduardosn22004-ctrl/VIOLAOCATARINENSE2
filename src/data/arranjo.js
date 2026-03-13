aacb0208452d5f1ec647675c7b59c65e82e833de
eduardosn22004-ctrl <eduardosn22004@gmail.com>
2026-03-13 17:42:36 -0300

    Update arranjo.js

--- src/data/arranjo.js
+++ src/data/arranjo.js
@@ -3,30 +3,30 @@ export const catalogo = [
     id: "darcy-villa-verde",
     compositor: "Darcy Villa Verde",
     obras: [
-      { titulo: "Foi ela - Ary Barroso", ano: "", formacao: "todas peças são arranjos", linkYoutube: "" },
-      { titulo: "Eu Sonhei Que Tu Estavas Tão Linda", ano: "", formacao: "f barroso,(faleceu)", linkYoutube: "" },
-      { titulo: "A Felicidade - Tom Jobim", ano: "", formacao: "Darcy Villa Verde / Foi Ela / Eu Sonhei Que Tu Estavas Tão / Felicidade / Se Tu Soubesses", linkYoutube: "" },
-      { titulo: "Se Tu Soubesse - cristóvão de alencar", ano: "", formacao: "VCFZ 89 - Dagoberto Linhares, Darcy Villa Verde (2007)", linkYoutube: "" }
+      { titulo: "Foi ela - Ary Barroso", ano: "", formacao: "", linkYoutube: "" },
+      { titulo: "Eu Sonhei Que Tu Estavas Tão Linda", ano: "", formacao: "", linkYoutube: "" },
+      { titulo: "A Felicidade - Tom Jobim", ano: "", formacao: "", linkYoutube: "" },
+      { titulo: "Se Tu Soubesse - cristóvão de alencar", ano: "", formacao: "", linkYoutube: "" }
     ]
   },
 {
     id: "ricardob",
     compositor: "Ricardo Boppre",
     obras: [
-      { titulo: "O Velinho", ano: "", formacao: "(arranjo)", linkYoutube: "https://youtu.be/c4C3aFmF27M?si=3KJQ0b0cG2YUWzct" },
-      { titulo: "O Holy Night", ano: "", formacao: "(arranjo)", linkYoutube: "https://youtu.be/7faW2fTG9c0?si=hdl72HT8kmLV53GC" },
-      { titulo: "Noite Feliz", ano: "", formacao: "(arranjo)", linkYoutube: "https://youtu.be/JffDw32h7Mo?si=em5Cu6hK2c6jK4UF" },
-      { titulo: "Natal das Crianças", ano: "", formacao: "(arranjo)", linkYoutube: "https://youtu.be/7RkakszxvB8?si=qr42HlJdnlAdRFAA" },
-      { titulo: "Jingle Bells", ano: "", formacao: "(arranjo)", linkYoutube: "https://youtu.be/HmsZTyzvWHY?si=JbYApNQEv_y4rkgE" },
-      { titulo: "Hark the Herald Angels Sing", ano: "", formacao: "(arranjo)", linkYoutube: "https://youtu.be/AfR3JZFI00o?si=QxOIb2tY8oa7E1N-" },
-      { titulo: "Como é Belo o Natal", ano: "", formacao: "(arranjo)", linkYoutube: "https://youtu.be/v--9FExRTF8?si=fASE6NKtfikh8qLW" },
+      { titulo: "O Velinho", ano: "", formacao: "", linkYoutube: "https://youtu.be/c4C3aFmF27M?si=3KJQ0b0cG2YUWzct" },
+      { titulo: "O Holy Night", ano: "", formacao: "", linkYoutube: "https://youtu.be/7faW2fTG9c0?si=hdl72HT8kmLV53GC" },
+      { titulo: "Noite Feliz", ano: "", formacao: "", linkYoutube: "https://youtu.be/JffDw32h7Mo?si=em5Cu6hK2c6jK4UF" },
+      { titulo: "Natal das Crianças", ano: "", formacao: "", linkYoutube: "https://youtu.be/7RkakszxvB8?si=qr42HlJdnlAdRFAA" },
+      { titulo: "Jingle Bells", ano: "", formacao: "", linkYoutube: "https://youtu.be/HmsZTyzvWHY?si=JbYApNQEv_y4rkgE" },
+      { titulo: "Hark the Herald Angels Sing", ano: "", formacao: "", linkYoutube: "https://youtu.be/AfR3JZFI00o?si=QxOIb2tY8oa7E1N-" },
+      { titulo: "Como é Belo o Natal", ano: "", formacao: "", linkYoutube: "https://youtu.be/v--9FExRTF8?si=fASE6NKtfikh8qLW" },
   ]
   },
 {
     id: "vitorgar",
     compositor: "Vitor Garbelotto",
     obras: [
-      { titulo: "Radamés Gnattalli", ano: "2010", formacao: "todas peças sao arranjos", linkYoutube: "" },
+      { titulo: "Radamés Gnattalli", ano: "2010", formacao: "", linkYoutube: "" },
     ]
   }
   ]  
